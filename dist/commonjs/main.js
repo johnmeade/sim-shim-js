9c34c9a56501d0703baee46d272a6da06a498472
John Meade <jon.meadee@gmail.com>
2016-07-12 12:06:45 -0400

    Revert "big refactor, added kelvin wave"
    This reverts commit bab06ca046db3b331a06cb4fc5df57b9d22d379d.

--- dist/commonjs/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require('./THREE-bundle.js');
-
-require('./math.js');
-
-require('./sim-shim-util');
-
-require('./sim-shim');
--- src/main.es6
+++
@@ -1,5 +0,0 @@
-import './THREE-bundle.js';
-import './math.js';
-// internal
-import './sim-shim-util';
-import './sim-shim';
